1707ab2a196aabfdf24c497abb749510f53ea55b
sébastien hamel <sebastienhamel@Mac.lan>
2025-12-08 10:19:03 -0500

    Fix Vite build config for multi-page and update links to relative

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ export default defineConfig({
     // Si vous déployez sur https://<USERNAME>.github.io/<REPO>/
     // changez la base ci-dessous par '/<REPO>/'
     base: './',
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                beta: 'beta.html',
+                sampler: 'sampler.html',
+            },
+        },
+    },
 })
